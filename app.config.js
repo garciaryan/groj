dec0316b402b02b61c503c283ac5e771f6ecbe54
Ryan Garcia <ryangarcia@Ryans-Mac-mini.attlocal.net>
2022-01-24 21:40:54 -0800

    added local json

---
+++ app.config.js
@@ -0,0 +1,8 @@
+export default {
+    name: 'Groj',
+    version: '0.0.1',
+    extra: {
+        carmdKey: 'Basic MTFlMDYwNDUtYWJhYy00OGEwLWIyMGEtYjE5YzgwZThmMmU0',
+        carmdToken: 'a13110bdf7c749d5b8d25ed791a13b73'
+    }
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,24 @@
 import { View, Text, Button, StyleSheet } from 'react-native';
 import React, { Component } from 'react';
+import { Image, Card } from 'react-native-elements';
+import vehicleData from '../api/vehicle_example.json';
+import vehicleImage from '../api/vehicle_image.json';
 
 class HomeScreen extends Component {
+    componentDidMount() {
+        console.log(vehicleData);
+        console.log(vehicleImage);
+    }
+
     render() {
+        const { make, model, year } = vehicleData.data;
         return (
             <View style={styles.container}>
                 <Text style={styles.title}>My Car</Text>
+                <Card>
+                    <Card.Title>{ year } {make} {model}</Card.Title>
+                    <Card.Image source={vehicleImage.data.image}></Card.Image>
+                </Card>
             </View>
         );
     }
